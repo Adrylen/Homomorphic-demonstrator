6c731a4f1227c47720d6560322ece127767af067
Adrylen <adrien.martinez@isen.yncrea.fr>
2017-11-22 15:18:06 +0100

    Mise en header du filtre

--- traitement Images/filter.cpp
+++ traitement Images/filter.cpp
@@ -1,76 +1,44 @@
-#include <vector>
-#include <string>
-#include <iostream>
+#include "filter.h"
 
-using namespace std;
-
-#ifndef FILTER_H
-#define FILTER_H
-class Filter
+Filter::Filter(int height, int width, vector<int> values)
 {
-	public :
+	filterHeight = height;
+	filterWidth = width;
+	filterValues = values;
+}
 
-	Filter(int height, int width, vector<int> values)
+
+void Filter::print()
+{
+	for(int i=0; i<filterHeight; i++)
 	{
-		filterHeight = height;
-		filterWidth = width;
-		filterValues = values;
-	}
-
-
-	void print()
-	{
-		for(int i=0; i<filterHeight; i++)
+		for(int j=0; j<filterWidth; j++)
 		{
-			for(int j=0; j<filterWidth; j++)
-			{
-				cout << filterValues[i*filterWidth+j] << " ";
-			}
-			cout << endl;
+			cout << filterValues[i*filterWidth+j] << " ";
 		}
+		cout << endl;
 	}
+}
 
-	int getValue(int x, int y)
+int Filter::getValue(int x, int y)
+{
+	if(x > filterHeight - 1)
+		throw invalid_argument("x over height of filter matrix");
+	if(y > filterWidth - 1)
+		throw invalid_argument("y over width of filter matrix");
+
+	return filterValues[x*filterWidth+y];
+}
+
+
+float Filter::getNorm()
+{
+	int sum = 0;
+
+	for(int i=0; i<filterValues.size(); i++)
 	{
-		if(x > filterHeight - 1)
-			throw invalid_argument("x over height of filter matrix");
-		if(y > filterWidth - 1)
-			throw invalid_argument("y over width of filter matrix");
-
-		return filterValues[x*filterWidth+y];
+		sum += filterValues[i];
 	}
 
-	int getHeight()
-	{
-		return filterHeight;
-	}
-
-	int getWidth()
-	{
-		return filterWidth;
-	}
-
-	float getNorm()
-	{
-		int sum = 0;
-
-		for(int i=0; i<filterValues.size(); i++)
-		{
-			sum += filterValues[i];
-		}
-
-		return (float) 1/sum;
-	}
-
-	bool validate()
-	{
-		return (filterHeight%2 == 1) && (filterWidth%2 == 1);
-	}
-	
-
-	private :
-		int filterHeight;
-		int filterWidth;
-		vector<int> filterValues;
-};
-#endif	//FILTER_H
+	return (float) 1/sum;
+}
